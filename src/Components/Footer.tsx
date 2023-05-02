a6a15d306f2840a11ace21e3e51ed00fab0694a7
And1 <92379947+And1x@users.noreply.github.com>
2023-05-02 23:50:24 +0200

    static img src

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,8 +1,10 @@
+import githubIcon from "/src/assets/img/GitHub-Mark-32px.png";
+
 export default function Footer() {
   return (
     <div className="flex justify-center items-center h-11 bg-emerald-800 mt-auto">
       <img
-        src="/src/assets/img/GitHub-Mark-32px.png"
+        src={githubIcon}
         alt="Github-Logo"
         className="object-contain h-6 w-auto mr-1"
       />
