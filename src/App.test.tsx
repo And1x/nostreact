be0d63e1022c1a5ea5b5e7998dfb3500b0bbb911
And1 <92379947+And1x@users.noreply.github.com>
2023-05-10 15:02:00 +0200

    setup vitest

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+it("should have hello", () => {
+  render(<App />);
+
+  const message = screen.queryByText(/Hello World/i);
+  expect(message).toBeVisible();
+});
