8ffe99eafcf18a6dbdb9f8c3a6bf8c43d3a7f15b
And1 <92379947+And1x@users.noreply.github.com>
2023-05-11 17:27:22 +0200

    tests for App and routing

--- src/App.tsx
+++ src/App.tsx
@@ -21,27 +21,22 @@ const relayUrls = [
 
 function App() {
   return (
-    // <NostrProvider relayUrls={relayUrls} debug={true}>
-    //   <BrowserRouter>
-    //     <div className="flex flex-col min-h-screen gap-8 bg-gray-800">
-    //       <Navbar />
-    //       <div className="flex justify-center">
-    //         <Routes>
-    //           <Route path="/" element={<Home />} />
-    //           <Route path="/resources" element={<Resources />} />
-    //           <Route path="/p/:pubkey" element={<ProfileFull />} />
-    //           <Route path="/n/:noteID" element={<NoteList />} />
-    //           <Route path="/tbd/:query" element={<ComingSoon />} />
-    //           <Route path="*" element={<NotFound />} />
-    //         </Routes>
-    //       </div>
-    //       <Footer />
-    //     </div>
-    //   </BrowserRouter>
-    // </NostrProvider>
-    <div>
-      <h1 className="invisible">Hello World</h1>
-    </div>
+    <NostrProvider relayUrls={relayUrls} debug={true}>
+      <div className="flex flex-col min-h-screen gap-8 bg-gray-800">
+        <Navbar />
+        <div className="flex justify-center">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/p/:pubkey" element={<ProfileFull />} />
+            <Route path="/n/:noteID" element={<NoteList />} />
+            <Route path="/tbd/:query" element={<ComingSoon />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </NostrProvider>
   );
 }
 
