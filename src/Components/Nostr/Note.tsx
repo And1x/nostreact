24f933e672cad3ba026f53aa6ce9699c86d792e5
And1 <92379947+And1x@users.noreply.github.com>
2023-05-25 15:16:59 +0200

    notes more-btn added

--- src/Components/Nostr/Note.tsx
+++ src/Components/Nostr/Note.tsx
@@ -13,6 +13,9 @@ type Probs = {
 
 export default function Note({ event, replies }: Probs) {
   const [showModal, setShowModal] = useState(false);
+  const [showMore, setShowMore] = useState(false);
+  const noteId = nip19.noteEncode(event.id);
+
   const onClose = () => {
     setShowModal(false);
   };
@@ -47,7 +50,7 @@ export default function Note({ event, replies }: Probs) {
 
       <div className="flex justify-between border-t border-emerald-600 mt-2 pt-2">
         <Link
-          to={`/n/${nip19.noteEncode(event.id)}`}
+          to={`/n/${noteId}`}
           reloadDocument
           className="flex text-white font-normal gap-1 fill-current hover:text-orange-500"
         >
@@ -56,24 +59,47 @@ export default function Note({ event, replies }: Probs) {
             viewBox="0 -960 960 960"
             className="w-6 h-6"
           >
+            <title>Replies</title>
             <path d="M880-80 720-240H140q-24 0-42-18t-18-42v-520q0-24 18-42t42-18h680q24 0 42 18t18 42v740ZM140-300v-520 520Zm606 0 74 80v-600H140v520h606Z" />
           </svg>
 
           {replies}
         </Link>
 
-        <button onClick={() => setShowModal(true)}>JSON</button>
-        <svg
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-6 h-6 fill-white hover:fill-orange-500"
-          xlinkTitle="more"
-        >
-          <path
-            d="M8,12a2,2,0,1,1-2-2A2,2,0,0,1,8,12Zm10-2a2,2,0,1,0,2,2A2,2,0,0,0,18,10Zm-6,0a2,2,0,1,0,2,2A2,2,0,0,0,12,10Z"
-            id="Horizontal"
-          ></path>
-        </svg>
+        <div onClick={() => setShowMore(!showMore)} className="relative">
+          <svg
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6 fill-white hover:fill-orange-500 hover:cursor-pointer"
+          >
+            <title>More...</title>
+            <path
+              d="M8,12a2,2,0,1,1-2-2A2,2,0,0,1,8,12Zm10-2a2,2,0,1,0,2,2A2,2,0,0,0,18,10Zm-6,0a2,2,0,1,0,2,2A2,2,0,0,0,12,10Z"
+              id="Horizontal"
+            ></path>
+          </svg>
+
+          <div
+            className={`${
+              showMore ? "visible" : "hidden"
+            } absolute w-24 z-10 bg-gray-200 rounded-lg shadow-lg text-black text-sm  font-normal p-1`}
+          >
+            <ul>
+              <li className="hover:text-orange-500 mb-1">
+                <button onClick={() => setShowModal(true)}>JSON</button>
+              </li>
+              <li className="hover:text-orange-500 mb-1">
+                <button
+                  onClick={() => {
+                    navigator.clipboard.writeText(noteId);
+                  }}
+                >
+                  Copy Note ID
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
